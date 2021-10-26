ae32297e6ab153707afb186f303ccf6b5c6a18f0
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-10-26 11:50:17 +0700

    add login with github

---
+++ app/Http/Controllers/Auth/SocialController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\SocialAccount;
+use App\Models\User;
+use Exception;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        try {
+            $social_user = Socialite::driver($provider)->user();
+            // First Find Social Account
+            $account = SocialAccount::with('user')->where([
+                'provider_name' => $provider,
+                'provider_id' => $social_user->getId()
+            ])->first();
+
+            // If Social Account Exist then Find User and Login
+            if ($account) {
+                auth()->login($account->user);
+                return redirect()->route('home');
+            }
+
+            // check user exist or create if user is not exist
+            $user = User::firstOrCreate([
+                'email' => $social_user->getEmail()
+            ], [
+                'email' => $social_user->getEmail(),
+                'name' => $social_user->getName()
+            ]);
+
+            // Create Social Accounts
+            $user->socialAccounts()->create([
+                'provider_id' => $social_user->getId(),
+                'provider_name' => $provider
+            ]);
+
+            // Login
+            auth()->login($user);
+            return redirect()->route('home');
+        } catch (Exception $e) {
+            return redirect()->route('login');
+        }
+    }
+}
--- config/services.php
+++ config/services.php
@@ -46,4 +46,10 @@ return [
         'redirect' => env('GOOGLE_URL'),
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' =>  env('GITHUB_URL'),
+    ],
+
 ];
